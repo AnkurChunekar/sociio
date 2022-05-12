c6b8f73fbed3176ecda2bdac766b231ebbf6f87b
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-05-12 22:58:49 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 import { makeServer } from "./server";
+import "./index.css";
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
