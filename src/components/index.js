23290bab40397b5fe1cfabc6c82c723a33edd863
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-05-15 13:33:48 +0530

    Fix: remove sidenavbar

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 export { Navbar } from "./Navbar";
-export { SideNavbar } from "./SideNavbar";
 export {PostCard} from "./PostCard";
 export {SuggestionSidebar} from "./SuggestionSidebar";
 export {EditProfileModal} from "./EditProfileModal";
