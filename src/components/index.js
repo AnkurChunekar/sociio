d5027c7c6c7e47794a6a0251b05bc3a4c48a4c02
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-05-15 13:31:49 +0530

    Feat: Add EditProfileModal

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { Navbar } from "./Navbar";
+export { SideNavbar } from "./SideNavbar";
+export {PostCard} from "./PostCard";
+export {SuggestionSidebar} from "./SuggestionSidebar";
+export {EditProfileModal} from "./EditProfileModal";
