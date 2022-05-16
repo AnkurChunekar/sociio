35063ee6ee5965111cf01ff860602784040ff167
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-05-16 14:23:10 +0530

    Feat: Add login functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,6 @@
-import { Link as ReachLink } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link as ReachLink, useNavigate } from "react-router-dom";
 import {
   Flex,
   Box,
@@ -13,10 +15,50 @@ import {
   Text,
   useColorModeValue,
 } from "@chakra-ui/react";
+import { login } from "redux/asyncThunks";
 
 export function Login() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { isLoading, token } = useSelector((state) => state.auth);
+
+  const [userData, setUserData] = useState({
+    username: "",
+    password: "",
+    toBeRemembered: false,
+  });
+
+  const updateUserData = (e) => {
+    setUserData((ud) => ({ ...ud, [e.target.name]: e.target.value }));
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const { payload } = await dispatch(login(userData));
+
+    if (payload.status === 200 && userData.toBeRemembered) {
+      localStorage.setItem("user", JSON.stringify(payload.data.foundUser));
+      localStorage.setItem("token", payload.data.encodedToken);
+    }
+  };
+
+  const handleGuestLogin = () => {
+    setUserData({
+      username: "adarshbalika",
+      password: "adarshBalika123",
+      toBeRemembered: true,
+    });
+  };
+
+  useEffect(() => {
+    if (token) navigate("/home");
+  }, [token, navigate]);
+
   return (
     <Flex
+      as="form"
+      onSubmit={handleLogin}
       minH={"calc(100vh - 120px)"}
       align={"center"}
       justify={"center"}
@@ -36,30 +78,64 @@ export function Login() {
           p={8}
         >
           <Stack spacing={4}>
-            <FormControl id="email">
-              <FormLabel>Email address</FormLabel>
-              <Input type="email" />
+            <FormControl id="username">
+              <FormLabel>username</FormLabel>
+              <Input
+                value={userData.username}
+                name="username"
+                onChange={updateUserData}
+                type="text"
+                isRequired
+              />
             </FormControl>
             <FormControl id="password">
               <FormLabel>Password</FormLabel>
-              <Input type="password" />
+              <Input
+                name="password"
+                value={userData.password}
+                onChange={updateUserData}
+                type="password"
+                isRequired
+              />
             </FormControl>
-            <Stack spacing={10}>
+            <Stack spacing={5}>
               <Stack
                 direction={{ base: "column", sm: "row" }}
                 align={"start"}
                 justify={"space-between"}
               >
-                <Checkbox>Remember me</Checkbox>
+                <Checkbox
+                  type="checkbox"
+                  alignSelf="flex-start"
+                  isChecked={userData.toBeRemembered}
+                  onChange={() =>
+                    setUserData((ud) => ({
+                      ...ud,
+                      toBeRemembered: !ud.toBeRemembered,
+                    }))
+                  }
+                >
+                  Remember me
+                </Checkbox>
               </Stack>
               <Button
+                type="submit"
                 bg={"blue.400"}
                 color={"white"}
                 _hover={{
                   bg: "blue.500",
                 }}
+                isLoading={isLoading}
               >
                 Sign in
+              </Button>
+              <Button
+                type="button"
+                onClick={handleGuestLogin}
+                colorScheme={"blue"}
+                variant={"outline"}
+              >
+                Use Guest Credentials
               </Button>
             </Stack>
           </Stack>
