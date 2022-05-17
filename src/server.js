dcafc2907a2c92d2e2a7bc63a5de46d4841728fc
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-05-17 14:52:37 +0530

    Fix: allow external routes

--- src/server.js
+++ src/server.js
@@ -51,6 +51,12 @@ export function makeServer({ environment = "development" } = {}) {
 
     routes() {
       this.namespace = "api";
+
+      // Allow external URLs to pass through
+      this.passthrough(
+        "https://api.cloudinary.com/v1_1/sociio/image/upload"
+      );
+
       // auth routes (public)
       this.post("/auth/signup", signupHandler.bind(this));
       this.post("/auth/login", loginHandler.bind(this));
