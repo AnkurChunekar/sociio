1d2779828485b69fb4c33a1086516e40dd32150b
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-05-18 16:32:42 +0530

    Feat: Add Like and bookmark thunks

--- src/redux/asyncThunks/index.js
+++ src/redux/asyncThunks/index.js
@@ -1,3 +1,12 @@
 export { login, signup, editUser } from "./authThunks";
 export { getAllUsers, followUser, unfollowUser } from "./usersThunks";
-export { createPost, getAllPosts, deletePost, editPost } from "./postsThunks";
+export {
+  createPost,
+  getAllPosts,
+  deletePost,
+  editPost,
+  likePost,
+  dislikePost,
+  removePostFromBookmark,
+  bookmarkPost,
+} from "./postsThunks";
