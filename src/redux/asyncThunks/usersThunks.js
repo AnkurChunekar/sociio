df9bc184ca9f72d50128228ca55e5d1cacf9edb9
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-05-18 15:40:57 +0530

    Feat: Add async thunks for Follow/unfollow functionality

--- src/redux/asyncThunks/usersThunks.js
+++ src/redux/asyncThunks/usersThunks.js
@@ -10,3 +10,43 @@ export const getAllUsers = createAsyncThunk("users/getAllUsers", async () => {
     console.error(error);
   }
 });
+
+export const followUser = createAsyncThunk(
+  "users/follow",
+  async ({ followUserId, token }, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(
+        `/api/users/follow/${followUserId}`,
+        {},
+        { headers: { authorization: token } }
+      );
+      const data = { data: response.data, status: response.status };
+      return data;
+    } catch (error) {
+      return rejectWithValue({
+        data: error.response.data,
+        status: error.response.status,
+      });
+    }
+  }
+);
+
+export const unfollowUser = createAsyncThunk(
+  "users/unfollow",
+  async ({ followUserId, token }, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(
+        `/api/users/unfollow/${followUserId}`,
+        {},
+        { headers: { authorization: token } }
+      );
+      const data = { data: response.data, status: response.status };
+      return data;
+    } catch (error) {
+      return rejectWithValue({
+        data: error.response.data,
+        status: error.response.status,
+      });
+    }
+  }
+);
