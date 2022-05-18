d273f98eee481a92239dd509bc278e240339c962
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-05-18 15:41:38 +0530

    Feat: update users slice with follow/unfollow reducers

--- src/redux/asyncThunks/index.js
+++ src/redux/asyncThunks/index.js
@@ -1,3 +1,3 @@
 export { login, signup, editUser } from "./authThunks";
-export { getAllUsers } from "./usersThunks";
+export { getAllUsers, followUser, unfollowUser } from "./usersThunks";
 export { createPost, getAllPosts, deletePost, editPost } from "./postsThunks";
--- src/redux/slices/usersSlice.js
+++ src/redux/slices/usersSlice.js
@@ -1,8 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { getAllUsers } from "redux/asyncThunks";
+import { getAllUsers,  followUser, unfollowUser } from "redux/asyncThunks";
 
 const initialState = {
   usersData: [],
+  status: "idle",
 };
 
 const usersSlice = createSlice({
@@ -12,9 +13,33 @@ const usersSlice = createSlice({
   extraReducers: {
     [getAllUsers.fulfilled]: (state, action) => {
       state.usersData = action.payload.data.users;
+      state.status = "succeeded";
     },
-    [getAllUsers.rejected]: (action) => {
+    [getAllUsers.rejected]: (state, action) => {
+      state.status = "failed";
       console.error(action);
+    },
+    [followUser.pending]: (state) => {
+      state.status = "loading";
+    },
+    [followUser.fulfilled]: (state, action) => {
+      state.status = "succeeded";
+      state.users = action.payload.data.users;
+    },
+    [followUser.rejected]: (state, action) => {
+      state.status = "failed";
+      console.error(action.payload.data.errors[0]);
+    },
+    [unfollowUser.pending]: (state) => {
+      state.status = "loading";
+    },
+    [unfollowUser.fulfilled]: (state, action) => {
+      state.status = "succeeded";
+      state.users = action.payload.data.users;
+    },
+    [unfollowUser.rejected]: (state, action) => {
+      state.status = "failed";
+      console.error(action.payload.data.errors[0]);
     },
   },
 });
