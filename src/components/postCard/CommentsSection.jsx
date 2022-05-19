e51ac5ceea04a0ace2a82dfdf62d37601bd40545
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-05-19 10:06:52 +0530

    Refactor: Separate component for comments section

---
+++ src/components/postCard/CommentsSection.jsx
@@ -0,0 +1,215 @@
+import { useState } from "react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Flex,
+  HStack,
+  IconButton,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+  Tooltip,
+  VStack,
+} from "@chakra-ui/react";
+import { FaEllipsisV } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { addComment, deleteComment, editComment } from "redux/asyncThunks";
+import { MdDownloadDone, MdOutlineCancel } from "react-icons/md";
+
+export const CommentsSection = ({ postData }) => {
+  const [commentInput, setCommentInput] = useState({
+    text: "",
+    editMode: false,
+    editText: "",
+    isLoading: false,
+    commentId: "",
+  });
+  const dispatch = useDispatch();
+  const { user, token } = useSelector((state) => state.auth);
+
+  const postCommentClickHandler = async () => {
+    if (commentInput.text.trim() !== "") {
+      setCommentInput((prev) => ({ ...prev, text: "", isLoading: true }));
+      await dispatch(
+        addComment({
+          postId: postData._id,
+          commentData: { text: commentInput.text },
+          token,
+        })
+      );
+      setCommentInput((prev) => ({ ...prev, isLoading: false }));
+    }
+  };
+
+  // { postId, commentId, token, commentData }
+  const saveEditedCommentClick = async (commentId) => {
+    if (commentInput.editText.trim() !== "") {
+      setCommentInput((prev) => ({
+        ...prev,
+        editMode: false,
+        editText: "",
+        isLoading: true,
+      }));
+      await dispatch(
+        editComment({
+          postId: postData._id,
+          commentId,
+          token,
+          commentData: { text: commentInput.editText },
+        })
+      );
+      setCommentInput((prev) => ({ ...prev, isLoading: false }));
+    }
+  };
+
+  return (
+    <>
+      <HStack width={"full"} px="1">
+        <Input
+          autoFocus
+          value={commentInput.text}
+          isDisabled={commentInput.isLoading}
+          onChange={(e) =>
+            setCommentInput((prev) => ({ ...prev, text: e.target.value }))
+          }
+          flexGrow={1}
+          placeholder="Add a comment"
+          size="sm"
+        />
+        <Button
+          isDisabled={commentInput.isLoading}
+          isLoading={commentInput.isLoading}
+          onClick={postCommentClickHandler}
+          variant={"ghost"}
+          colorScheme="blue"
+          size="sm"
+        >
+          Post
+        </Button>
+      </HStack>
+
+      {postData.comments.map((item) => (
+        <Flex
+          w="100%"
+          key={item._id}
+          alignItems={"flex-start"}
+          flexGrow="1"
+          px="2"
+          gap="3"
+        >
+          <Avatar
+            size="xs"
+            mt="1"
+            src={
+              item.username === user.username ? user.avatarURL : item.avatarURL
+            }
+          />
+          <VStack alignItems={"flex-start"} spacing="sm" flexGrow={2}>
+            <Text wordBreak={"break-word"} fontWeight={"600"} fontSize="14px">
+              {item.firstName + " " + item.lastName}
+            </Text>
+            {commentInput.editMode && item._id === commentInput.commentId ? (
+              <InputGroup flexGrow={"2"} size="md">
+                <Input
+                  autoFocus
+                  variant="flushed"
+                  value={commentInput.editText}
+                  onChange={(e) =>
+                    setCommentInput((prev) => ({
+                      ...prev,
+                      editText: e.target.value,
+                    }))
+                  }
+                  pr="4.5rem"
+                  type="text"
+                />
+                <InputRightElement width="4.5rem">
+                  <Tooltip label="Save">
+                    <IconButton
+                      onClick={() => saveEditedCommentClick(item._id)}
+                      borderRadius={"full"}
+                      bg="transparent"
+                      icon={<MdDownloadDone size={"20px"} />}
+                    />
+                  </Tooltip>
+                  <Tooltip label="Cancel">
+                    <IconButton
+                      borderRadius={"full"}
+                      bg="transparent"
+                      onClick={() =>
+                        setCommentInput((prev) => ({
+                          ...prev,
+                          editText: item.text,
+                          editMode: false,
+                        }))
+                      }
+                      icon={<MdOutlineCancel size="20px" />}
+                    />
+                  </Tooltip>
+                </InputRightElement>
+              </InputGroup>
+            ) : (
+              <Text flexGrow={"2"} wordBreak={"break-word"} fontSize="14px">
+                {item.text}
+              </Text>
+            )}
+          </VStack>
+
+          {item.username === user.username && !commentInput.editMode ? (
+            <Menu>
+              <MenuButton
+                as={Box}
+                cursor="pointer"
+                _hover={{ bg: "gray.200" }}
+                borderRadius="full"
+                p="1"
+              >
+                <IconButton
+                  isDisabled={commentInput.isLoading}
+                  isLoading={commentInput.isLoading}
+                  aria-label="Open Comment Menu"
+                  backgroundColor={"transparent"}
+                  size={"sm"}
+                  icon={<FaEllipsisV />}
+                />
+              </MenuButton>
+              <MenuList minWidth="140px">
+                <MenuItem
+                  onClick={() =>
+                    setCommentInput((prev) => ({
+                      ...prev,
+                      editMode: true,
+                      editText: item.text,
+                      commentId: item._id,
+                    }))
+                  }
+                >
+                  Edit
+                </MenuItem>
+                <MenuItem
+                  onClick={() =>
+                    dispatch(
+                      deleteComment({
+                        postId: postData._id,
+                        commentId: item._id,
+                        token,
+                      })
+                    )
+                  }
+                >
+                  Delete
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          ) : null}
+        </Flex>
+      ))}
+    </>
+  );
+};
---
+++ src/components/postCard/PostCard.jsx
@@ -0,0 +1,217 @@
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link as ReachLink } from "react-router-dom";
+import {
+  Avatar,
+  Flex,
+  IconButton,
+  Text,
+  VStack,
+  HStack,
+  Box,
+  Tooltip,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  useDisclosure,
+  Link,
+} from "@chakra-ui/react";
+import { AiOutlineEllipsis, AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { BsBookmark, BsBookmarkFill } from "react-icons/bs";
+import { VscComment } from "react-icons/vsc";
+import {
+  bookmarkPost,
+  deletePost,
+  dislikePost,
+  getAuthUser,
+  likePost,
+  removePostFromBookmark,
+} from "redux/asyncThunks";
+import { PostModal } from "../PostModal";
+import { CommentsSection } from "./CommentsSection";
+// import { getPostCommentsService } from "services";
+
+export const PostCard = ({ postData }) => {
+  const { user, token } = useSelector((state) => state.auth);
+  const { onOpen, isOpen, onClose } = useDisclosure();
+  const [showCommentsSection, setShowCommentsSection] = useState(false);
+  const dispatch = useDispatch();
+  if (postData === undefined) {
+    return;
+  }
+
+  const isCurrentUsersPost = user.username === postData.username;
+  const avatarURL =
+    postData.username === user.username ? user.avatarURL : postData.avatarURL;
+
+  // Like Functionality
+  const isLikedByUser = postData.likes.likedBy.some(
+    (item) => item.username === user.username
+  );
+
+  const likeClickHandler = () => {
+    if (isLikedByUser) {
+      dispatch(dislikePost({ postId: postData._id, token }));
+    } else {
+      dispatch(likePost({ postId: postData._id, token }));
+    }
+  };
+
+  // Bookmark functionality
+  const isBookmarkedByUser = user.bookmarks.some((id) => id === postData._id);
+
+  const bookmarkClickHandler = async () => {
+    if (isBookmarkedByUser) {
+      await dispatch(removePostFromBookmark({ postId: postData._id, token }));
+      dispatch(getAuthUser({ username: user.username }));
+    } else {
+      await dispatch(bookmarkPost({ postId: postData._id, token }));
+      dispatch(getAuthUser({ username: user.username }));
+    }
+  };
+
+  return (
+    <VStack
+      p="4"
+      gap={2}
+      maxW="500px"
+      alignItems={"flex-start"}
+      borderRadius={"xl"}
+      bg="white"
+      flexGrow={1}
+      w="100%"
+    >
+      <Flex
+        alignItems={"center"}
+        justifyContent="space-between"
+        gap="2"
+        w="full"
+        px="2"
+      >
+        <Link
+          as={ReachLink}
+          display={"flex"}
+          gap="2"
+          alignItems="center"
+          _hover={{ textDecoration: "none" }}
+          to={`/profile/${postData.username}`}
+        >
+          <Avatar size="sm" src={avatarURL} />
+          <HStack alignItems={"center"} flexGrow="1" flexWrap={"wrap"}>
+            <Text fontWeight={"600"} fontSize="lg">
+              {postData.firstName + " " + postData.lastName}
+            </Text>
+            <Text color="var(--chakra-colors-gray-500)">
+              @{postData.username}
+            </Text>
+          </HStack>
+        </Link>
+
+        {/* ellipsis menu */}
+        {isCurrentUsersPost ? (
+          <Menu>
+            <MenuButton
+              as={Box}
+              cursor="pointer"
+              _hover={{ bg: "gray.200" }}
+              borderRadius="full"
+              p="1"
+            >
+              <IconButton
+                aria-label="Open Menu"
+                backgroundColor={"transparent"}
+                marginLeft={"auto"}
+                fontSize="30px"
+                size={"xs"}
+                icon={<AiOutlineEllipsis />}
+              />
+            </MenuButton>
+            <MenuList minWidth="140px">
+              <MenuItem onClick={onOpen}>Edit</MenuItem>
+              <MenuItem
+                onClick={() =>
+                  dispatch(deletePost({ postId: postData._id, token }))
+                }
+              >
+                Delete
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        ) : null}
+
+        {/* ellipsis menu */}
+      </Flex>
+      <Text px="2">{postData.content}</Text>
+
+      <HStack alignItems={"center"} gap="50px" w="full" px="2">
+        <HStack>
+          <Tooltip label="Like" fontSize="md">
+            <IconButton
+              onClick={likeClickHandler}
+              // isDisabled={likeStatus === "loading"}
+              backgroundColor={"transparent"}
+              color={isLikedByUser ? "red.500" : "black"}
+              fontSize="22px"
+              size={"xs"}
+              py="2"
+              borderRadius={"full"}
+              icon={isLikedByUser ? <AiFillHeart /> : <AiOutlineHeart />}
+            />
+          </Tooltip>
+          <Text>{postData.likes.likeCount}</Text>
+        </HStack>
+
+        <HStack>
+          <Tooltip label="Comment" fontSize="md">
+            <IconButton
+              onClick={() => setShowCommentsSection((prev) => !prev)}
+              backgroundColor={"transparent"}
+              fontSize="22px"
+              size={"xs"}
+              py="2"
+              borderRadius={"full"}
+              icon={<VscComment />}
+            />
+          </Tooltip>
+          <Text>{postData.comments.length}</Text>
+        </HStack>
+
+        <HStack>
+          <Tooltip label="Bookmark" fontSize="md">
+            <IconButton
+              onClick={bookmarkClickHandler}
+              backgroundColor={"transparent"}
+              color={isBookmarkedByUser ? "blue.400" : "black"}
+              fontSize="20px"
+              size={"xs"}
+              py="2"
+              borderRadius={"full"}
+              icon={isBookmarkedByUser ? <BsBookmarkFill /> : <BsBookmark />}
+            />
+          </Tooltip>
+        </HStack>
+      </HStack>
+
+      {postData.likes.likeCount > 0 ? (
+        <Text color={"gray"} fontSize={"sm"} px="2">
+          Liked by {postData.likes.likedBy[0].username}
+          {postData.likes.likeCount > 1
+            ? ` and ${postData.likes.likeCount - 1} others`
+            : ""}
+        </Text>
+      ) : null}
+
+      {showCommentsSection ? <CommentsSection postData={postData} /> : null}
+
+      {isCurrentUsersPost ? (
+        <PostModal
+          isOpen={isOpen}
+          onClose={onClose}
+          editMode={true}
+          editPostData={postData}
+        />
+      ) : null}
+    </VStack>
+  );
+};
