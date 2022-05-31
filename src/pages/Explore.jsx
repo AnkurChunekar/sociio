5953a370f8384a1c68691868fdf3504bee303a9e
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-06-01 00:37:31 +0530

    Feat: Infinite Scroll on Explore page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,28 +1,79 @@
-import { Fragment, useState } from "react";
-import { useSelector } from "react-redux";
-import { VStack, Text } from "@chakra-ui/react";
-import { PostCard, FilterRow, SearchBar } from "components";
-import { getFilteredPosts } from "helpers/filterHelpers";
+import { useEffect, useRef, useState, useCallback } from "react";
+import { VStack, Text, Box, Spinner } from "@chakra-ui/react";
+import { getCurrentPagedPosts } from "services/getCurrentPagedPosts.service";
+import { PostCard } from "components";
 
 export const Explore = () => {
-  const [sortByValue, setSortByValue] = useState("");
-  const { posts } = useSelector((state) => state.posts);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [infiniteScrollStatus, setInfinteScrollStatus] = useState({
+    loading: false,
+    completed: false,
+  });
+  const targetRef = useRef(null);
+  const [pagedPosts, setPagedPosts] = useState([]);
 
-  const filteredPosts = getFilteredPosts(posts, sortByValue);
+  const getNewPosts = useCallback(async () => {
+    setInfinteScrollStatus((prev) => ({ ...prev, loading: true }));
+    const newPosts = await getCurrentPagedPosts(currentPage);
+    if (newPosts) {
+      setPagedPosts((prev) => {
+        return [...prev, ...newPosts];
+      });
+    } else {
+      setInfinteScrollStatus({ loading: false, completed: true });
+      intersectionObserver.current.unobserve(targetRef.current);
+      return;
+    }
+    setInfinteScrollStatus((prev) => ({ ...prev, loading: false }));
+  }, [currentPage]);
+
+  const intersectionObserver = useRef(
+    new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting && !infiniteScrollStatus.loading) {
+          setCurrentPage((prev) => prev + 1);
+        }
+      },
+      {
+        threshold: 0.1,
+      }
+    )
+  );
+
+  useEffect(() => {
+    intersectionObserver.current.observe(targetRef.current);
+  }, []);
+
+  useEffect(() => {
+    if (currentPage > 0) {
+      getNewPosts();
+    }
+  }, [currentPage, getNewPosts]);
 
   return (
-    <VStack gap={5}>
-      <SearchBar />
-       <FilterRow sortByValue={sortByValue} setSortByValue={setSortByValue} />
-      {filteredPosts.length > 0 ? (
-        filteredPosts.map((item) => (
-          <Fragment key={item._id}>
-            <PostCard postData={item} />
-          </Fragment>
-        ))
-      ) : (
-        <Text>There are no posts to display.</Text>
-      )}
-    </VStack>
+    <>
+      <VStack gap={5}>
+        {pagedPosts.length > 0 ? (
+          pagedPosts.map((item) => <PostCard key={item._id} postData={item} />)
+        ) : (
+          <Text>There are no posts to display.</Text>
+        )}
+        {infiniteScrollStatus.loading ? (
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="blue.500"
+            size="xl"
+          />
+        ) : null}
+        {infiniteScrollStatus.completed ? (
+          <Text fontSize={"lg"} fontWeight="600">
+            You're All Caught Up!😎
+          </Text>
+        ) : null}
+        <Box h={"50px"} w="full" ref={targetRef}></Box>
+      </VStack>
+    </>
   );
 };
